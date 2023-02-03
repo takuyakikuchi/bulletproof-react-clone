4c5ebf4b6ff24d9a7d7c15a923321a04ad73e185
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-03 09:59:58 +0900

    [fix] fix lint error with react scope

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        // To allow module.exports
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
@@ -20,5 +22,11 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        /**
+         * Fix "React must be in scope when using JSX"
+         * @see https://ja.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint
+         */
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off"
     }
 }
