e752517b5f7a21ed8e448472f41daf12d7dbca7b
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-03 11:07:03 +0900

    [chore] install headless-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { Switch } from '@headlessui/react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className={`${
+        enabled ? 'bg-blue-600' : 'bg-gray-200'
+      } relative inline-flex h-6 w-11 items-center rounded-full`}
+    >
+      <span className="sr-only">Enable notifications</span>
+      <span
+        className={`${
+          enabled ? 'translate-x-6' : 'translate-x-1'
+        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+      />
+    </Switch>
+  );
 }
 
 export default App;
