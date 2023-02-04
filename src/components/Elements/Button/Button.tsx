af5aa36c3495c33dcbbad468145c835a3c388398
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-04 12:12:08 +0900

    [feat] Landing, Button, Spinner UI

--- src/components/Elements/Button/Button.tsx
+++ src/components/Elements/Button/Button.tsx
@@ -1,11 +1,58 @@
-type Props = {
-  children: React.ReactNode;
+import clsx from 'clsx';
+import { Spinner } from '@/components/Elements/Spinner';
+
+const variants = {
+  primary: 'bg-blue-600 text-white',
+  inverse: 'bg-white text-blue-600',
+  danger: 'bg-red-600 text-white',
 };
 
-export const Button = ({ children }: Props) => {
+const sizes = {
+  sm: 'py-2 px-1 text-sm',
+  md: 'py-2 px-6 text-md',
+  lg: 'py-3 px-8 text-lg',
+};
+
+type IconProps =
+  | { startIcon: React.ReactElement; endIcon?: never }
+  | { startIcon?: never; endIcon: React.ReactElement }
+  | { startIcon?: undefined; endIcon?: undefined };
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: keyof typeof variants;
+  size?: keyof typeof sizes;
+  isLoading?: boolean;
+} & IconProps;
+
+export const Button = ({
+  type = 'button',
+  variant = 'primary',
+  size = 'md',
+  className = '',
+  isLoading = false,
+  startIcon,
+  endIcon,
+  children,
+  ...props
+}: Props) => {
   return (
-    <button>
+    <button
+      // TODO: Uncomment when understood why ref is used here.
+      // ref={ref}
+      type={type}
+      className={clsx(
+        'flex justify-center items-center border border-gray-300 rounded-md shadow-sm font-medium',
+        'disabled:opacity-70 disabled:cursor-not-allowed focus:outline-none hover:opacity-80',
+        variants[variant],
+        sizes[size],
+        className,
+        { ...props }
+      )}
+    >
+      {isLoading && <Spinner size="sm" className="text-current" />}
+      {!isLoading && startIcon}
       <span className="mx-2">{children}</span>
+      {!isLoading && endIcon}
     </button>
   );
 };
