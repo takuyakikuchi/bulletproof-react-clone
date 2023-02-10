32a077fc243bd2a7e5e798e449e33e8012bc0100
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-10 15:14:22 +0900

    [feat] UserNavigation UI

--- src/components/Layout/PageLayout/parts/Topbar.tsx
+++ src/components/Layout/PageLayout/parts/Topbar.tsx
@@ -1,3 +1,13 @@
+import { UserNavigation } from './UserNavigation';
+
 export const Topbar = () => {
-  return <div className="flex-shrink-0 h-16 bg-white shadow"></div>;
+  return (
+    <div className="flex-shrink-0 h-16 bg-white shadow">
+      <div className="flex-1 px-4 flex justify-end h-full">
+        <div className="ml-4 flex items-center md:ml-6">
+          <UserNavigation />
+        </div>
+      </div>
+    </div>
+  );
 };
