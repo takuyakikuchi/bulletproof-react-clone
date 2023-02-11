3ecd36770cfab63b56845c33c56e198524effbd3
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-11 18:51:43 +0900

    [test] mock cleanup

--- src/components/Form/__tests__/Form.test.tsx
+++ src/components/Form/__tests__/Form.test.tsx
@@ -12,65 +12,75 @@ const schema = z.object({
   title: z.string(),
 });
 
-it('should render and submit a basic Form component', async () => {
-  const user = userEvent.setup();
-  const handleSubmit = vi.fn();
+describe('Form', () => {
+  /**
+   * Register a callback to be called after each one of the tests in the current context completes.
+   * @see https://vitest.dev/api/#aftereach
+   */
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
 
-  await render(
-    <Form<typeof testData, typeof schema>
-      onSubmit={handleSubmit}
-      schema={schema}
-      id="my-form"
-    >
-      {({ register, formState }) => (
-        <>
-          <InputField
-            label="Title"
-            error={formState.errors.title}
-            registration={register('title')}
-          />
-          <Button name="submit" type="submit" className="w-full">
-            Submit
-          </Button>
-        </>
-      )}
-    </Form>
-  );
+  it('should render and submit a basic Form component', async () => {
+    const user = userEvent.setup();
+    const handleSubmit = vi.fn();
 
-  await user.type(screen.getByLabelText(/title/i), testData.title);
-  await user.click(screen.getByRole('button', { name: /submit/i }));
-  waitFor(() =>
-    expect(handleSubmit).toHaveBeenCalledWith(testData, expect.anything())
-  );
-});
+    await render(
+      <Form<typeof testData, typeof schema>
+        onSubmit={handleSubmit}
+        schema={schema}
+        id="my-form"
+      >
+        {({ register, formState }) => (
+          <>
+            <InputField
+              label="Title"
+              error={formState.errors.title}
+              registration={register('title')}
+            />
+            <Button name="submit" type="submit" className="w-full">
+              Submit
+            </Button>
+          </>
+        )}
+      </Form>
+    );
 
-it('should fail submission if valiation fails', async () => {
-  const user = userEvent.setup();
-  const handleSubmit = vi.fn();
+    await user.type(screen.getByLabelText(/title/i), testData.title);
+    await user.click(screen.getByRole('button', { name: /submit/i }));
+    waitFor(() =>
+      expect(handleSubmit).toHaveBeenCalledWith(testData, expect.anything())
+    );
+  });
 
-  await render(
-    <Form<typeof testData, typeof schema>
-      onSubmit={handleSubmit}
-      schema={schema}
-      id="my-form"
-    >
-      {({ register, formState }) => (
-        <>
-          <InputField
-            label="Title"
-            error={formState.errors.title}
-            registration={register('title')}
-          />
+  it('should fail submission if valiation fails', async () => {
+    const user = userEvent.setup();
+    const handleSubmit = vi.fn();
 
-          <Button name="submit" type="submit" className="w-full">
-            Submit
-          </Button>
-        </>
-      )}
-    </Form>
-  );
+    await render(
+      <Form<typeof testData, typeof schema>
+        onSubmit={handleSubmit}
+        schema={schema}
+        id="my-form"
+      >
+        {({ register, formState }) => (
+          <>
+            <InputField
+              label="Title"
+              error={formState.errors.title}
+              registration={register('title')}
+            />
 
-  await user.click(screen.getByRole('button', { name: /submit/i }));
-  screen.findByRole(/alert/i), { name: /required/i };
-  expect(handleSubmit).not.toHaveBeenCalled();
+            <Button name="submit" type="submit" className="w-full">
+              Submit
+            </Button>
+          </>
+        )}
+      </Form>
+    );
+
+    await user.click(screen.getByRole('button', { name: /submit/i }));
+    screen.findByRole(/alert/i), { name: /required/i };
+    expect(handleSubmit).not.toHaveBeenCalled();
+  });
 });
