a98be421f061172be93f6dec937dd740d644b6a1
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-12 15:16:13 +0900

    [fix] UserNavigation goes under the content

--- src/components/Layout/PageLayout/parts/Topbar.tsx
+++ src/components/Layout/PageLayout/parts/Topbar.tsx
@@ -4,7 +4,8 @@ export const Topbar = () => {
   return (
     <div className="flex-shrink-0 h-16 bg-white shadow">
       <div className="flex-1 px-4 flex justify-end h-full">
-        <div className="ml-4 flex items-center md:ml-6">
+        {/* Without this z-10 the dropdown menu goes under the main contents */}
+        <div className="ml-4 flex items-center md:ml-6 z-10">
           <UserNavigation />
         </div>
       </div>
