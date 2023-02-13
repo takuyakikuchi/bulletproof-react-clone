f8a76afc0fc23b63f24d27f5892e9ac8dc96cb34
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-13 15:53:30 +0900

    [feat] Drawer takes props

--- src/components/Form/FormDrawer.tsx
+++ src/components/Form/FormDrawer.tsx
@@ -1,5 +1,26 @@
-import { Drawer } from '@/components/Elements';
+import { Drawer, Button } from '@/components/Elements';
+
+const Footer = (
+  <>
+    <Button variant="inverse" size="sm" onClick={() => null}>
+      Cancel
+    </Button>
+    <Button form="update-profile" type="submit" size="sm" isLoading={false}>
+      Submit
+    </Button>
+  </>
+);
 
 export const FormDrawer = () => {
-  return <Drawer />;
+  return (
+    <Drawer
+      isOpen={true}
+      onClose={() => null}
+      title={'Update Profile'}
+      size={'md'}
+      footer={Footer}
+    >
+      Children
+    </Drawer>
+  );
 };
