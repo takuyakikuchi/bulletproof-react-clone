43f6486723feb88ed097f0f9dfd685d7f8300f7c
Takuya Kikuchi <takuyakikuch@gmail.com>
2023-02-26 14:55:56 +0900

    [test] use AAA pattern

--- src/hooks/__tests__/useDisclosure.test.ts
+++ src/hooks/__tests__/useDisclosure.test.ts
@@ -1,45 +1,67 @@
 import { renderHook, act } from '@testing-library/react';
 import { useDisclosure } from '../useDisclosure';
 
-test('should define initial state', () => {
-  // Using the default prop value
-  const { result } = renderHook(() => useDisclosure());
-  expect(result.current.isOpen).toBe(false);
+describe('useDisclosure', () => {
+  it('should initialize isOpen false as default', () => {
+    // Arrange
+    const { result } = renderHook(() => useDisclosure());
 
-  const { result: resultTwo } = renderHook(() => useDisclosure(true));
-  expect(resultTwo.current.isOpen).toBe(true);
-});
-
-test('open should make isOpen true', () => {
-  const { result } = renderHook(() => useDisclosure());
-  
-  act(() => {
-    result.current.open();
+    // Assert
+    expect(result.current.isOpen).toBe(false);
   });
 
-  expect(result.current.isOpen).toBe(true);
-});
+  it('should initialize isOpen with the value passed with the prop', () => {
+    // Arrange
+    const { result } = renderHook(() => useDisclosure(true));
 
-test('close should make isOpen false', () => {
-  const { result } = renderHook(() => useDisclosure(true));
-
-  act(() => {
-    result.current.close();
+    // Assert
+    expect(result.current.isOpen).toBe(true);
   });
 
-  expect(result.current.isOpen).toBe(false);
-});
+  it('should open isOpen', () => {
+    // Arrange
+    const { result } = renderHook(() => useDisclosure());
+    
+    // Act
+    act(() => {
+      result.current.open();
+    });
 
-test('toggle should reverse isOpen value', () => {
-  const { result } = renderHook(() => useDisclosure());
-
-  act(() => {
-    result.current.toggle();
+    // Assert
+    expect(result.current.isOpen).toBe(true);
   });
-  expect(result.current.isOpen).toBe(true);
 
-  act(() => {
-    result.current.toggle();
+  it('should close isOpen', () => {
+    // Arrange
+    const { result } = renderHook(() => useDisclosure(true));
+
+    // Act
+    act(() => {
+      result.current.close();
+    });
+
+    // Assert
+    expect(result.current.isOpen).toBe(false);
   });
-  expect(result.current.isOpen).toBe(false);
+
+  it('should toggle isOpen', () => {
+    // Arrange
+    const { result } = renderHook(() => useDisclosure());
+
+    // Act
+    act(() => {
+      result.current.toggle();
+    });
+
+    // Assert
+    expect(result.current.isOpen).toBe(true);
+
+    // Act
+    act(() => {
+      result.current.toggle();
+    });
+
+    // Assert
+    expect(result.current.isOpen).toBe(false);
+  });
 });
